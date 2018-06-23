e26a4c154de633ca89ef5898ff3100eb0df160ba
Nitai <benshaul.n@gmail.com>
2018-06-23 22:35:55 +0300

    NavBar and EventDashboard components added to project

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
+import NavBar from '../../features/nav/NavBar/NavBar'
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Welcome to App</h1>
+        <NavBar />
+   <EventDashboard />
       </div>
     );
   }
